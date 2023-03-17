c3d1e5b5a81d012224402a3397c47c608a139111
margot_shupe <jmargotromov@gmail.com>
2023-03-17 19:19:16 -0400

    trying the GET request

--- GOT/index.js
+++ GOT/index.js
@@ -1,2 +1,12 @@
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
 const app = express();
+
+app.listen(8000, () => console.log("Winter is comming"));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  console.log();
+
+  res.send("You know nothing");
+});
