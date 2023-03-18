d9c9f8019cfd97e2eccf5e9bfd34df3e0365cbb0
margot_shupe <jmargotromov@gmail.com>
2023-03-17 22:20:55 -0400

    working on my seed

---
+++ GOT/db/seed.js
@@ -0,0 +1,7 @@
+// import models
+import mongoose from "./connection";
+import Charactersg from "../models/Charactersg.js";
+import Episodes from "../models/Episodes.js";
+// import our json data
+import charactersgdata from "../db/charactersg.json" assert { type: "json" };
+import episodesdata from "../db/episodes.json" assert { type: "json" };
---
+++ GOT/models/Episodes.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const EpisodesSchema = new mongoose.Schema({
+  episodes: [
+    {
+      seasonNum: Number,
+      episodeNum: Number,
+      episodeTitle: String,
+      episodeDescription: String,
+      openingSequenceLocations: [
+        {
+          type: String,
+        },
+      ],
+    },
+  ],
+});
+
+export default mongoose.model("Episodes", EpisodesSchema);
+
+/*{
+   "episodes":[
+      {
+         "seasonNum":1,
+         "episodeNum":1,
+         "episodeTitle":"Winter Is Coming",
+         "episodeLink":"/title/tt1480055/",
+         "episodeAirDate":"2011-04-17",
+         "episodeDescription":"Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army.",
+         "openingSequenceLocations":[
+            "King's Landing",
+            "Winterfell",
+            "The Wall",
+            "Pentos"
+         ],
+*/
